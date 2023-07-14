7c3fb0967940cb2d660d81d8115a517dcb8fb966
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-07-14 10:35:30 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pyfaults',
-   version='0.0.3',
+   version='0.0.4',
    description='A useful module',
    author='Sinclair Combs',
    author_email='sinclaircombs@mines.edu',
