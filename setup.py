6f0f25333408ed6c262a134caa8946e1716c2757
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-07-14 10:50:17 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pyfaults',
-   version='0.0.8',
+   version='0.0.9',
    description='A useful module',
    author='Sinclair Combs',
    author_email='sinclaircombs@mines.edu',
