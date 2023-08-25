b6399e46070e1162fdefb042b3966939538b2493
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-08-25 11:14:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pyfaults',
-   version='1.1.5',
+   version='1.1.6',
    description='Software for examining non-layered structures with stacking faults',
    author='Sinclair Combs',
    author_email='sinclaircombs@mines.edu',
