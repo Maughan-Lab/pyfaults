4e0f1510cc0d3357aad7a849d9dfe1b0a1793009
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-09-05 15:31:18 -0600

    Add files via upload

--- pyfaults/autoSearch.py
+++ pyfaults/autoSearch.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 
-class autoSearch(object):
+class AutoSearch(object):
     
     # properties ------------------------------------------------------------------------
     expt = property(lambda self: self._expt)
