e1ce6d567cd91c4645fe6c7b6ab65599f79319ef
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-09-05 15:38:34 -0600

    Add files via upload

--- pyfaults/autoSearch.py
+++ pyfaults/autoSearch.py
@@ -3,15 +3,12 @@
 # Author: Sinclair R. Combs
 #########################################################################################
 
-import pandas as pd
-
 class AutoSearch(object):
     
     # properties ------------------------------------------------------------------------
     expt = property(lambda self: self._expt)
     
-    unitcell = property(lambda self: self._unitcell,
-                        lambda self, val: self.setUnitCell(unitcell=val))
+    unitcell = property(lambda self: self._unitcell)
     
     nStacks = property(lambda self: self._nStacks,
                        lambda self, val: self.setSearchParams(nStacks=val))
@@ -46,11 +43,11 @@ class AutoSearch(object):
     fitDiffList = property(lambda self: self._fitDiffList,
                             lambda self, val: self.fitDiffs(fitDiffList=val))
     
-    def __init__(self, expt):
+    def __init__(self, expt, unitcell):
         
         # initialize parameters
         self._expt = expt
-        self._unitcell = None
+        self._unitcell = unitcell
         self._nStacks = None
         self._fltLayer = None
         self._sVecList = None
@@ -70,64 +67,6 @@ class AutoSearch(object):
         
         self._fitDiffList = None
         self._fitDiffList = self.fitDiffs()
-        
-        return
-    
-    # sets unit cell from CSV file ------------------------------------------------------
-    def setUnitCell(self, path, fn, lattice, layerNames, stackDir):
-        from pyfaults.lattice import Lattice
-        from pyfaults.layerAtom import LayerAtom
-        from pyfaults.layer import Layer
-        from pyfaults.unitcell import Unitcell
-        
-        df = pd.read_csv(path + fn + ".csv")
-        
-        # set lattice parameters so stacking direction is along c
-        if stackDir == "a":
-            newLatt = Lattice(a=lattice.c, b=lattice.b, c=lattice.a,
-                              alpha=lattice.gamma, beta=lattice.beta, 
-                              gamma=lattice.alpha)
-        elif stackDir == "b":
-            newLatt = Lattice(a=lattice.a, b=lattice.c, c=lattice.b,
-                              alpha=lattice.alpha, beta=lattice.gamma, 
-                              gamma=lattice.beta)
-        elif stackDir == "c":
-            newLatt = Lattice(a=lattice.a, b=lattice.b, c=lattice.c,
-                              alpha=lattice.alpha, beta=lattice.beta, 
-                              gamma=lattice.gamma)
-        
-        # create layer objects
-        layers = []
-        for i in layerNames:
-            lyr = df[df["Layer"] == i]
-            
-            alist = []
-            for j in range(len(lyr.index)):
-                layerName = df.iloc[j]["Layer"]
-                atomLabel = df.iloc[j]["Atom"]
-                element = df.iloc[j]["Element"]
-                x = df.iloc[j]["x"]
-                y = df.iloc[j]["y"]
-                z = df.iloc[j]["z"]
-                occ = df.iloc[j]["Occupancy"]
-                
-                if stackDir == "a":
-                    newXYZ = [z, y, x]
-                elif stackDir == "b":
-                    newXYZ = [x, z, y]
-                elif stackDir == "c":
-                    newXYZ = [x, y, z]
-                
-                newAtom = LayerAtom(layerName, atomLabel, element, newXYZ, occ, newLatt)
-                alist.append(newAtom)    
-        
-            newLayer = Layer(alist, newLatt, i)
-            layers.append(newLayer)
-        
-        # create unit cell object
-        unitcell = Unitcell(fn, layers, newLatt)
-        
-        self._unitcell = unitcell
         
         return
 
