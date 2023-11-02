39737c3f653b51bc78c11530fbf73ec4624665bc
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-11-02 14:55:26 -0600

    Add files via upload

--- pyfaults/calcFitDiffs.py
+++ pyfaults/calcFitDiffs.py
@@ -2,7 +2,7 @@
 # Author: Sinclair R. Combs
 ##################################################################################
 
-def calcFitDiffs(simPath, diffPath, CSVName):
+def calcFitDiffs(simPath, diffPath):
     '''
     Parameters
     ----------
@@ -10,56 +10,31 @@ def calcFitDiffs(simPath, diffPath, CSVName):
         str : simulation data file directory
     diffPath
         str : difference curve data file directory
-    CSVName
-        str : file name of metadata CSV generated from calcDiffs method
-
-    Returns
-    -------
-    fitDiffData
-        DataFrame : tabulated fit diff data, includes the following as columns
-            'Model' -- unique identifier
-            'Q' -- Q values
-            'UF vs FLT' -- intensity differences between unfaulted model fit and
-            faulted model fit to experimental XRD
     '''
-    import os
-    import pandas as pd
+    import os, glob
     import numpy as np
     
     # create 'fitDiffCurves' folder in file directory
     if os.path.exists(simPath + 'fitDiffCurves/') == False:
         os.mkdir(simPath + 'fitDiffCurves/')
+    
+    # import difference curves
+    unfaultedDiffQ, unfaultedDiff = pf.importFile(diffPath, 'Unfaulted_sim_exptDiff')
+    diffCurves = glob.glob(diffPath + '/*.txt')
+    
+    for f in diffCurves:
+        removeExt = f.split('.')
+        fn = removeExt[0].split('\\')
         
-    diffDF = pd.read_csv(diffPath + CSVName + '.csv')
-    
-    Q = diffDF['Expt vs. Model Diff Q'][0]
-    
-    UFdiff = diffDF['Expt vs. Model Diff Intensity'][0]
-    
-    fitQ = []
-    fitDiffs = []
-    # calculate fit differences
-    for i in diffDF.index:
-        if i == 0:
-            fitDiffs.append('None')
-        elif i > 0:
-            name = diffDF['Model'][i]
-            modelDiff = diffDF['Expt vs. Model Diff Intensity'][i]
-            diff = np.subtract(UFdiff, modelDiff)
-            fitDiffs.append(diff)
-            fitQ.append(Q)
-            # save fit difference data
-            with open(simPath + 'fitDiffCurves/' + name + '_fitDiff.txt', 'w') as f:
-                for q, diff in range(len(Q, fitDiffs)):
-                    f.write('{0} {1}\n'.format(q, diff))
-            f.close() 
+        if fn[-1] is not 'Unfaulted_sim_exptDiff':
+            faultedDiffQ, faultedDiff = pf.importFile(diffPath, fn[-1])
+            fitDiff = np.subtract(unfaultedDiff, faultedDiff)
             
-    # store fit differences in data frame
-    fitDiffData = pd.dataFrame()
-    fitDiffData['Model'] = diffDF['Model']
-    fitDiffData['Q'] = fitQ
-    fitDiffData['UF vs FLT'] = fitDiffs
-    
-    fitDiffData.to_csv(simPath + 'fitDiffCurves/fitDiff_info.csv')
-    
-    return fitDiffData
+            
+            saveName = fn[-1].split('_')
+            with open(simPath + 'fitDiffCurves/' + saveName[0] + '_fitDiff.txt', 'w') as x:
+                for (d) in fitDiff:
+                    x.write('{0} \n'.format(d))
+                x.close()
+                
+    return
