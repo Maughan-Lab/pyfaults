9087c72094873d75ad0cea49382359a89b298287
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2024-06-30 18:33:47 -0600

    Add files via upload

--- pyfaults/__init__.py
+++ pyfaults/__init__.py
@@ -24,6 +24,7 @@ import pyfaults.analyze
 import pyfaults.supercellAdjZ
 import pyfaults.gridSearch
 import pyfaults.simulate
+import pyfaults.TMSupercell
 
 ''' TOP LEVEL METHODS '''
 #---------------------------------------------------------------------------------
