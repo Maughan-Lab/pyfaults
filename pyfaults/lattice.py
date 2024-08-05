43997c37cd65e06525f5aec4e8dc4074dfd25479
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2024-08-05 14:22:16 -0600

    Update lattice.py

--- pyfaults/lattice.py
+++ pyfaults/lattice.py
@@ -2,11 +2,19 @@
 # Author: Sinclair R. Combs
 ##################################################################################
 
-#---------------------------------------------------------------------------------
-# Lattice object class
-#---------------------------------------------------------------------------------
+# Lattice object class ----------
 class Lattice(object):
-    # properties -----------------------------------------------------------------
+'''
+Parameters
+----------
+a (float) -- unit cell vector a
+b (float) -- unit cell vector b
+c (float) -- unit cell vector c
+alpha (float) -- unit cell angle alpha
+beta (float) -- unit cell angle beta
+gamma (float) -- unit cell angle gamma
+'''
+    # properties ----------
     a =\
         property(lambda self: self._a,
                  lambda self, val: self.setParam(a=val),
@@ -37,9 +45,8 @@ class Lattice(object):
                  lambda self, val: self.setParam(gamma=val),
                  doc='float : unit cell angle gamma in degrees')
     
-    # creates instance of Lattice object -----------------------------------------
+    # initialization, defines Lattice defaults ----------
     def __init__(self, a, b, c, alpha, beta, gamma):
-        # initialize parameters
         self._a = None
         self._b = None
         self._c = None
@@ -49,7 +56,7 @@ class Lattice(object):
         self.setParam(a, b, c, alpha, beta, gamma)
         return
     
-    # sets parameters ------------------------------------------------------------
+    # sets parameters of Lattice ----------
     def setParam(self, a=None, b=None, c=None, alpha=None, beta=None, gamma=None):
         if a is not None:
             self._a = a
@@ -65,7 +72,7 @@ class Lattice(object):
             self._gamma = gamma
         return
     
-    # prints lattice parameters --------------------------------------------------
+    # prints lattice parameters ----------
     def display(self):
         displayStr = '\n'.join(('a : ' + str(self.a), 
                          'b : ' + str(self.b), 
@@ -74,4 +81,4 @@ class Lattice(object):
                          'beta : ' + str(self.beta),
                          'gamma : ' + str(self.gamma)))
         print(displayStr)
-        return displayStr
+        return
