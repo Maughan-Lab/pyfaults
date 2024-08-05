3c44c4238680f93227f9ef6228296933de527a82
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2024-08-05 17:01:09 -0600

    Update importCSV.py

--- pyfaults/importCSV.py
+++ pyfaults/importCSV.py
@@ -4,18 +4,18 @@
 
 # generates unit cell from CSV of atomic parameters ----------
 def importCSV(path, fn, lattParams, lyrNames):
-'''
-Parameters
-----------
-path (str) : CSV file location
-fn (str) : CSV file name
-lattParams (array_like) : lattice parameters [a, b, c, alpha, beta, gamma]
-lyrNames (array_like) : defined layer names
+    '''
+    Parameters
+    ----------
+    path (str) : CSV file location
+    fn (str) : CSV file name
+    lattParams (array_like) : lattice parameters [a, b, c, alpha, beta, gamma]
+    lyrNames (array_like) : defined layer names
 
-Returns
-----------
-unitcell (Unitcell) : new unit cell
-''' 
+    Returns
+    ----------
+    unitcell (Unitcell) : new unit cell
+    ''' 
     import pyfaults as pf
 
     csv = pd.read_csv(path + fn + '.csv')
